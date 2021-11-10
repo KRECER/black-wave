8608d3277b7dc613325644aadfca5ecc659d9a39
Forsythe1 <v_dmytriiev@ukr.net>
2021-11-10 02:31:29 +0200

    archive-projects-adaptive

---
+++ source/js/components/menu-categories.js
@@ -0,0 +1,3 @@
+document.querySelector('.menu-categories__item--has-dropdown').addEventListener('click', () => {
+document.querySelector('.menu-categories__sublist').classList.toggle('menu-categories__sublist--active');
+});
